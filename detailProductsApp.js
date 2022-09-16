5919f3142a3608f5e6985f964b4b8cd3e82d7746
zDreeDz <102372993+zDreeDz@users.noreply.github.com>
2022-09-16 14:03:05 +0300

    мобилки

--- detailProductsApp.js
+++ detailProductsApp.js
@@ -9,13 +9,18 @@ $(document).ready(function(){
         }
         });    
       $('.description-slider-items-bottom-img').slick({
-        //slidesToShow: 4,
+        slidesToShow: 5,
         // dots: false,
         // arrows:false,
         // dotsClass: 'ibc-slick-dots-detail',
         /*customPaging: function(slider, i) {      
             return '<div class="custom-slick-dots" id=' + i + "></div>";
         }*/
+        });
+    $(document).ready(function() {
+        $('.hedar-burger').click(function(event){
+            $('.hedar-burger,.bgmenu').toggleClass('active');
+        });
         });  
 
 });
